9a162c7ba477ab3f3b8986582089f033cd304c5f
liangliang.liu <liangliang.liu@qunar.com>
2023-07-20 15:25:26 +0800

    设置版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ java {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-//    version.set("2021.2")
+    version.set("2021.2")
 //    type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.java"))
